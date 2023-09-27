ee5375eb8ef4803e077cfa1f5419839e97fb43fa
arthihariharan <128405138+arthihariharan@users.noreply.github.com>
2023-09-27 10:57:22 -0400

    Update go.sh
    Changed name from project to 611

--- go.sh
+++ go.sh
@@ -2,5 +2,5 @@
 
 #Build docker container
 
-docker build . --build-arg USER_ID=$(id -u) -t project
-docker run --rm -v $(pwd):/home/rtudio/work -p 8787:8787 -it project
+docker build . --build-arg USER_ID=$(id -u) -t 611
+docker run --rm -v $(pwd):/home/rtudio/work -p 8787:8787 -it 611
